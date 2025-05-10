8d05b6bf1a4a21b4410ab091be56692938cc7aa2
Nəzər Abdullayev <160178320+NezerAbdullayev@users.noreply.github.com>
2025-05-10 09:55:48 +0000

    feat: update-useWhyDidYouRender-hook

--- src/hooks/useWhyDidYouRender.tsx
+++ src/hooks/useWhyDidYouRender.tsx
@@ -1,17 +1,44 @@
-import { useEffect, useRef } from "react";
-import { logChanges, shallowEqual } from "../helpers";
+export const logChanges = (name: string, prev: any, next: any) => {
+  const changes: Record<string, { from: any; to: any }> = {};
+  const allKeys = new Set([...Object.keys(prev), ...Object.keys(next)]);
 
-export const useWhyDidYouRender = (
-  componentName: string,
-  props: Record<string, any>
-) => {
-  const previousProps = useRef(props);
-
-  useEffect(() => {
-    if (!shallowEqual(previousProps.current, props)) {
-      logChanges(componentName, previousProps.current, props);
+  allKeys.forEach((key) => {
+    if (prev[key] !== next[key]) {
+      changes[key] = { from: prev[key], to: next[key] };
     }
-
-    previousProps.current = props;
   });
+
+  const changeKeys = Object.keys(changes);
+
+  let color = "";
+  if (changeKeys.length <= 4) {
+    color = "#ffcc80";
+  } else if (changeKeys.length <= 6) {
+    color = "#ff9800";
+  } else {
+    color = "#e65100";
+  }
+
+  // this is a special case for the "match" prop in react-router
+  if (
+    name === "Anonymous" &&
+    changeKeys.length === 1 &&
+    changeKeys[0] === "match"
+  ) {
+    return;
+  }
+
+  if (changeKeys.length > 0) {
+    console.log(
+      `%c[WDYR] ${name} (${changeKeys.length} changed)`,
+      `color: ${color}; font-weight: bold;`,
+      changes
+    );
+  } else {
+    // No actual changes but re-rendered
+    console.log(
+      `%c[WDYR] ${name} re-rendered with same props`,
+      "color: red; font-weight: bold;"
+    );
+  }
 };
